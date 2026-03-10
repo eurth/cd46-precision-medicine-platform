32c8cd19622633bb4c21dfae85d57a5ba75a64d0
CD46 Platform <research@cd46platform.local>
2026-03-11 00:24:31 +0530

    debug: add temporary IP debug page

---
+++ app/pages/98_debug_ip.py
@@ -0,0 +1,8 @@
+﻿"""
+Helper script to hit an external IP service to debug Streamlit Cloud proxy headers.
+"""
+import requests
+import streamlit as st
+
+st.write("Headers seen by app:")
+st.json(dict(st.context.headers))
